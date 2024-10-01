daf53352664919f45a3edfe9a895803338ab3335
mac.me <04044@proton.me>
2024-10-01 21:37:30 +0300

    added English language

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 const b = document.querySelector('.meow-facts---button-js')
 function meowFacts() {
     b.addEventListener('click', () => {
-    fetch('https://meowfacts.herokuapp.com/?lang=rus').then(responce => responce.json()).then(data => document.querySelector('.meow-facts---span-js').innerHTML = data.data + '.', document.querySelector('.meow-facts--h1-js').innerHTML = 'Meow Facts:').catch(error => console.log(error));
+    fetch('https://meowfacts.herokuapp.com/?lang=eng').then(responce => responce.json()).then(data => document.querySelector('.meow-facts---span-js').innerHTML = data.data + '.', document.querySelector('.meow-facts--h1-js').innerHTML = 'Meow Facts:').catch(error => console.log(error));
     })};
 meowFacts();
